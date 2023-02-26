b0195cfc2896d5c66e6ae1753fb8bc7baf95c777
Cireka <tsifekide@gmail.com>
2023-02-26 16:20:31 +0400

    added logic for displaying response and sent msgs and also logic for sending earlier msgs to api to we get better responses

--- Components/IdeaToText/IdeaToText.js
+++ Components/IdeaToText/IdeaToText.js
@@ -1,20 +1,68 @@
 import style from "./IdeaToText.module.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const IdeaToText = () => {
+  const [text, setText] = useState("");
+  const [responses, setResponses] = useState([]);
+  const GptKey = process.env.Gpt_Key;
+
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, [responses]);
+
+  useEffect(() => {
+    if (text !== "") {
+      axios
+        .post(
+          "https://api.openai.com/v1/completions",
+          {
+            model: "text-davinci-003",
+            prompt: `${responses.join("\n")}\n${text}`,
+            temperature: 0.9,
+            max_tokens: 450,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${GptKey}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          setResponses([...responses, text, res.data.choices[0].text]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [text]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      setText(event.target.value);
+    }
+  };
+
   return (
     <section className={style.Section}>
       <div className={style.RecivedTextParrent}>
-        <span className={style.RecivedText}>
-          In this example, we're using the Typewriter component to display the
-          text we receive from the GPT-3 API. When the user clicks the "Generate
-          Text" button, we make a request to the GPT-3 API using the axios
-          library. When we receive a response, we set the gptText state to the
-        </span>
+        {responses.map((text, index) => (
+          <span key={index} className={style.RecivedText}>
+            {text}
+          </span>
+        ))}
       </div>
       <div className={style.SendTextParrent}>
-        <textarea className={style.TextArea}></textarea>
+        <textarea onKeyDown={handleKeyDown} className={style.TextArea} />
       </div>
     </section>
   );
 };
+
 export default IdeaToText;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     Text_Speech: "86df1c445eb4015349761a2db30f423a",
+    Gpt_Key: "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",
   },
 };
 
