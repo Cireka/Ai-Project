babd54744820bb96a13184ba93883a8c667a98d5
Cireka <tsifekide@gmail.com>
2023-03-02 15:14:38 +0400

    Finished Basic Logic for text to image chat

--- src/pages/TextToImage/index.js
+++ src/pages/TextToImage/index.js
@@ -6,19 +6,23 @@ import axios from "axios";
 
 const TextToImagePage = () => {
   const [picture, setPicture] = useState([]);
-  const [imagePrompt, setImagePrompt] = useState("");
+  const [imagePrompt, setImagePrompt] = useState([]);
   const [loading, setLoading] = useState(false);
   const GptKey = process.env.GPT_KEY;
 
   useEffect(() => {
-    if (imagePrompt !== "") {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, [picture]);
+
+  useEffect(() => {
+    if (imagePrompt.length >= 1) {
       setLoading(true);
       axios
         .post(
           "https://api.openai.com/v1/images/generations",
           {
             model: "image-alpha-001",
-            prompt: imagePrompt,
+            prompt: imagePrompt.slice(-1)[0],
             num_images: 1,
             size: "256x256",
           },
@@ -43,28 +47,33 @@ const TextToImagePage = () => {
   }, [imagePrompt]);
 
   const handleKeyDown = (event) => {
-    if (event.key === "Enter" && !event.shiftKey) {
+    if (event.key === "Enter" && loading === false) {
       event.preventDefault();
-      setImagePrompt(event.target.value);
-      event.target.value = ""; // Clear the value of the textarea
+      setImagePrompt((previousImagePrompts) => [
+        ...previousImagePrompts,
+        event.target.value,
+      ]);
+      setTimeout(() => {
+        event.target.value = ""; // Clear the value of the textarea
+      }, 0);
     }
   };
 
-  console.log(picture);
+  console.log(imagePrompt);
 
   return (
     <Fragment>
       <Navigation />
       <section className={style.Section}>
         <div className={style.RecivedTextParrent}>
-          {/* {responses.map((text, index) => (
-          <span key={index} className={style.RecivedText}>
-            {text}
-          </span>
-        ))} */}
           {picture.length >= 1 &&
             picture.map((item, index) => {
-              return <img key={index} src={item.url} />;
+              return (
+                <div key={Math.random()} className={style.ChatParrent}>
+                  <h2 key={Math.random()}>{imagePrompt[index]}</h2>
+                  <img key={Math.random()} src={item.url} />
+                </div>
+              );
             })}
           {loading && <span className={style.RecivedText}>Loading...</span>}
         </div>
